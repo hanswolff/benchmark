430e10c881388b77747188bf1472a053496cc49f
Hans Wolff <hans.wolff@gmx.de>
2013-11-23 11:00:38 +0000

    created benchmark for non-persistend MemoryMappedFiles

---
+++ System.IO/MemoryMappedFilesTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System.Diagnostics;
+using System.IO.MemoryMappedFiles;
+
+namespace Benchmark.System.IO
+{
+    [TestFixture]
+    public class MemoryMappedFilesTests
+    {
+        const int Iterations = 10000;
+
+        [TestCase(1)]
+        [TestCase(1024)]
+        [TestCase(1024 * 1024)]
+        public void NonPersistentMemoryMappedFile_CreationDestruction_WithSize(int size)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            for (var i = 0; i < Iterations; i++)
+            {
+                using (MemoryMappedFile.CreateNew("testmap", size))
+                {
+                }
+            }
+            stopwatch.StopAndLog(Iterations);
+        }
+    }
+}
