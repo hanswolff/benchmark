1ba45449a6c9ad6134007590bb9083727d86fedb
Hans Wolff <hans.wolff@gmx.de>
2013-11-24 09:32:05 +0000

    ConcurrentQueue benchmark

---
+++ System.Collections/ConcurrentQueueTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Benchmark.System.Collections
+{
+    [TestFixture]
+    public class ConcurrentQueueTests
+    {
+        const int Iterations = 1000000;
+
+        [TestCase(1, 1)]
+        [TestCase(2, 1)]
+        [TestCase(1, 2)]
+        [TestCase(4, 1)]
+        [TestCase(1, 4)]
+        [TestCase(2, 2)]
+        [TestCase(4, 4)]
+        public void PushTryPop(int producerThreads, int consumerThreads)
+        {
+            var stack = new ConcurrentQueue<int>();
+            var startEvent = new ManualResetEventSlim(false);
+            var finished = 0;
+            var stop = false;
+            var producerTasks = Enumerable.Range(0, producerThreads).Select(i => Task.Factory.StartNew(() =>
+                {
+                    var count = Iterations/producerThreads;
+                    startEvent.Wait();
+                    for (var j = 0; j < count; j++)
+                        stack.Enqueue(0);
+                    Interlocked.Increment(ref finished);
+                    if (finished >= producerThreads) stop = true;
+                }, TaskCreationOptions.LongRunning)).ToArray();
+            var consumerTasks = Enumerable.Range(0, consumerThreads).Select(i => Task.Factory.StartNew(() =>
+                {
+                    int num;
+                    startEvent.Wait();
+                    while (!stop) stack.TryDequeue(out num);
+                }, TaskCreationOptions.LongRunning)).ToArray();
+
+            var stopwatch = Stopwatch.StartNew();
+            startEvent.Set();
+            stop = true;
+            Task.WaitAll(producerTasks);
+            Task.WaitAll(consumerTasks);
+            stopwatch.StopAndLog(Iterations);
+        }
+    }
+}
