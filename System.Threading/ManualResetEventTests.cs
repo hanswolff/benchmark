1ac043d2e368ba92dbddd5920666b602b93dec0a
Hans Wolff <hans.wolff@gmx.de>
2013-11-24 09:21:00 +0000

    ManualResetEvent Tests

---
+++ System.Threading/ManualResetEventTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Benchmark.System.Threading
+{
+    [TestFixture]
+    public class ManualResetEventTests
+    {
+        const int Iterations = 100000;
+
+        [Test]
+        public void CreationDestruction()
+        {
+            var stopwatch = Stopwatch.StartNew();
+            for (var i = 0; i < Iterations; i++)
+            {
+                using (new ManualResetEvent(false))
+                {
+                }
+            }
+            stopwatch.StopAndLog(Iterations);
+        }
+
+        [Test]
+        public void Set_Reset_Cycles()
+        {
+            using (var resetEvent = new ManualResetEvent(false))
+            {
+                var stopwatch = Stopwatch.StartNew();
+
+                for (var i = 0; i < Iterations; i++)
+                {
+                    resetEvent.Set();
+                    resetEvent.Reset();
+                }
+                stopwatch.StopAndLog(Iterations);
+            }
+        }
+
+        [Test]
+        public void WaitOne_AlreadySet()
+        {
+            using (var resetEvent = new ManualResetEvent(true))
+            {
+                var stopwatch = Stopwatch.StartNew();
+
+                for (var i = 0; i < Iterations; i++)
+                {
+                    resetEvent.WaitOne(0);
+                }
+                stopwatch.StopAndLog(Iterations);
+            }
+        }
+    }
+}
