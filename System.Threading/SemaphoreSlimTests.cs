2df6e6fa5424e02c1905b9179a6d4e6b21bb2fc4
Hans Wolff <hans.wolff@gmx.de>
2013-12-03 11:35:02 +0100

    added SemaphoreSlim benchmark

---
+++ System.Threading/SemaphoreSlimTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Benchmark.System.Threading
+{
+    [TestFixture]
+    public class SemaphoreSlimTests
+    {
+        const int Iterations = 1000000;
+
+        [Test]
+        public void CreationDestruction()
+        {
+            var stopwatch = Stopwatch.StartNew();
+            for (var i = 0; i < Iterations; i++)
+            {
+                using (new SemaphoreSlim(2, 2))
+                {
+                }
+            }
+            stopwatch.StopAndLog(Iterations);
+        }
+
+        [Test]
+        public void WaitRelease()
+        {
+            using (var semaphore = new SemaphoreSlim(2, 2))
+            {
+                var stopwatch = Stopwatch.StartNew();
+                for (var i = 0; i < Iterations; i++)
+                {
+                    semaphore.Wait();
+                    semaphore.Release();
+                }
+                stopwatch.StopAndLog(Iterations);
+            }
+        }
+    }
+}
