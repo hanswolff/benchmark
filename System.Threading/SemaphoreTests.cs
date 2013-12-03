881ca4b918a8c7968ae1637ee57a2505c3aadaf4
Hans Wolff <hans.wolff@gmx.de>
2013-12-03 11:33:49 +0100

    added Semaphore benchmark

---
+++ System.Threading/SemaphoreTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Benchmark.System.Threading
+{
+    [TestFixture]
+    public class SemaphoreTests
+    {
+        const int Iterations = 100000;
+
+        [Test]
+        public void CreationDestruction()
+        {
+            var stopwatch = Stopwatch.StartNew();
+            for (var i = 0; i < Iterations; i++)
+            {
+                using (new Semaphore(2, 2))
+                {
+                }
+            }
+            stopwatch.StopAndLog(Iterations);
+        }
+
+        [Test]
+        public void WaitRelease()
+        {
+            using (var semaphore = new Semaphore(2, 2))
+            {
+                var stopwatch = Stopwatch.StartNew();
+                for (var i = 0; i < Iterations; i++)
+                {
+                    semaphore.WaitOne();
+                    semaphore.Release();
+                }
+                stopwatch.StopAndLog(Iterations);
+            }
+        }
+    }
+}
