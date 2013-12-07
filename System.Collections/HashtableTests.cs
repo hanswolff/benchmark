52116207aa5a7d3c3ae67770ccb1225c794f1398
Hans Wolff <hans.wolff.home@gmail.com>
2013-12-07 16:21:17 +0100

    added Hashtable benchmark

--- System.Collections/HashtableTests.cs
+++ System.Collections/HashtableTests.cs
@@ -1,12 +1,12 @@
 ﻿using NUnit.Framework;
-using System.Collections.Generic;
+using System.Collections;
 using System.Diagnostics;
 using System.Linq;
 
 namespace Benchmark.System.Collections
 {
     [TestFixture]
-    public class DictionaryTests
+    public class HashtableTests
     {
         const int Iterations = 2000000;
 
@@ -14,7 +14,7 @@ namespace Benchmark.System.Collections
         public void Add_Key_Int32()
         {
             var stopwatch = Stopwatch.StartNew();
-            var hashSet = new Dictionary<int, object>();
+            var hashSet = new Hashtable();
             for (var i = 0; i < Iterations; i++)
             {
                 hashSet.Add(i, null);
@@ -26,7 +26,7 @@ namespace Benchmark.System.Collections
         public void Add_Key_Int64()
         {
             var stopwatch = Stopwatch.StartNew();
-            var hashSet = new Dictionary<long, object>();
+            var hashSet = new Hashtable();
             for (long i = 0; i < Iterations; i++)
             {
                 hashSet.Add(i, null);
@@ -37,7 +37,7 @@ namespace Benchmark.System.Collections
         [Test]
         public void ForEach_Key_Int32()
         {
-            var list = Enumerable.Range(0, Iterations).ToDictionary(x => x, null);
+            var list = new Hashtable(Enumerable.Range(0, Iterations).ToDictionary(x => x, null));
 
             var stopwatch = Stopwatch.StartNew();
             foreach (var item in list)
@@ -49,7 +49,7 @@ namespace Benchmark.System.Collections
         [Test]
         public void ForEach_Key_Int64()
         {
-            var list = Enumerable.Range(0, Iterations).ToDictionary(x => (long)x, null);
+            var list = new Hashtable(Enumerable.Range(0, Iterations).ToDictionary(x => x, null));
 
             var stopwatch = Stopwatch.StartNew();
             foreach (var item in list)
@@ -61,7 +61,7 @@ namespace Benchmark.System.Collections
         [Test]
         public void RemoveLast_Key_Int32()
         {
-            var list = Enumerable.Range(0, Iterations).ToDictionary(x => x, null);
+            var list = new Hashtable(Enumerable.Range(0, Iterations).ToDictionary(x => x, null));
 
             var stopwatch = Stopwatch.StartNew();
             while (list.Count > 0) list.Remove(list.Count - 1);
@@ -71,7 +71,7 @@ namespace Benchmark.System.Collections
         [Test]
         public void RemoveLast_Key_Int64()
         {
-            var list = Enumerable.Range(0, Iterations).ToDictionary(x => (long)x, null);
+            var list = new Hashtable(Enumerable.Range(0, Iterations).ToDictionary(x => x, null));
 
             var stopwatch = Stopwatch.StartNew();
             while (list.Count > 0) list.Remove(list.Count - 1);
